1191a506057b1fcd955783db5263dce3386730cf
gegebe <ggirbes@gmail.com>
2023-10-20 12:26:45 +0200

    3 functions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
 // Tu código aquí.
+let lowerButton = document.querySelector(".prevBtn");
+let addButton = document.querySelector(".nextBtn");
+let number = document.querySelector("#counter");
+let counter = 0;
+
+function addCounter(){
+  counter++;
+  number.textContent = counter;
+  updateColour();
+}
+
+function removeCounter(){
+  counter--;
+  number.textContent = counter;
+  updateColour();
+}
+
+function updateColour(){
+  if(number.textContent > 0){
+    number.style.color = "green";
+  }else if(number.textContent < 0){
+    number.style.color = "red";
+  }else{
+    number.style.color = "black";
+  }
+}
+
+
+lowerButton.addEventListener("click",removeCounter);
+addButton.addEventListener("click", addCounter);
